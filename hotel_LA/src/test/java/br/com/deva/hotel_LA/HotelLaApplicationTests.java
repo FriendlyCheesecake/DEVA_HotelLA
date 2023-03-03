844df0c7ee09ec528d66e83faa5b7be700165728
Amanda Fantuzze <105981624+FriendlyCheesecake@users.noreply.github.com>
2023-03-03 11:48:06 -0300

    PrimeroCommit

---
+++ hotel_LA/src/test/java/br/com/deva/hotel_LA/HotelLaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deva.hotel_LA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelLaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
