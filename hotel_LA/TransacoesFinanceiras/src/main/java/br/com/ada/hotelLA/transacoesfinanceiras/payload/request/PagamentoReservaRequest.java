768f0a9c1cc408501a1052dedff2c1e2cf93e777
AneteS <anete.koga@gmail.com>
2023-03-07 02:22:30 -0300

    Request do Pagamento de uma Reserva

--- hotel_LA/TransacoesFinanceiras/src/main/java/br/com/ada/hotelLA/transacoesfinanceiras/payload/request/PagamentoReservaRequest.java
+++ hotel_LA/TransacoesFinanceiras/src/main/java/br/com/ada/hotelLA/transacoesfinanceiras/payload/request/PagamentoReservaRequest.java
@@ -3,13 +3,17 @@ package br.com.ada.hotelLA.transacoesfinanceiras.payload.request;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Data
 public class PagamentoReservaRequest {
+    @NotNull
+    private Quarto quarto;
 
-    //NÃO DEFINI ESTAS VARIÁVEIS:
-    //@NotNull
-    //private ReservaRequest reserva;
+    @NotNull
+    private List<QuartosDisponiveis> quartosDisponiveis;
 
-    //@NotNull
-    //private List<ReservasRealizadas> reservas;
 }
